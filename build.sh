c7ace9e2736a0394c33a8092d0adb29c60f4c896
jaychandra <jaychandradasbapu05@gmail.com>
2026-08-19 20:31:48 +0530

    Create superuser via shell to bypass password validators

--- build.sh
+++ build.sh
@@ -11,5 +11,16 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
-  python manage.py createsuperuser --noinput || true
+  python manage.py shell -c "
+import os
+from django.contrib.auth.models import User
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
+else:
+    print('Superuser already exists or credentials missing; skipping.')
+"
 fi
